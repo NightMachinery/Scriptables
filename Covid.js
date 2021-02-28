e42d7846ae60b7ae86036d24ce2cb7ae80f44f69
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2021-02-28 23:22:52 +0330

    Repo started

---
+++ Covid.js
@@ -0,0 +1,57 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: light-gray; icon-glyph: magic;
+// change "country" to a value from https://coronavirus-19-api.herokuapp.com/countries/
+const country = "Iran"
+const url = `https://coronavirus-19-api.herokuapp.com/countries/${country}`
+const req = new Request(url)
+const res = await req.loadJSON()
+
+if (config.runsInWidget) {
+  let w = new ListWidget()
+  w.backgroundColor = new Color("#53d769")
+  let preTxt = w.addText("Covid")
+  preTxt.textColor = Color.white()
+  preTxt.textOpacity = 0.8
+  preTxt.font = Font.systemFont(16)
+  w.addSpacer(5)
+  let titleTxt = w.addText(`${res.todayDeaths} deaths`)
+  titleTxt.textColor = Color.white()
+  titleTxt.font = Font.systemFont(22)
+  w.addSpacer(5)
+  let subTxt = w.addText(`${res.todayCases} cases`)
+  subTxt.textColor = Color.white()
+  subTxt.textOpacity = 0.8
+  subTxt.font = Font.systemFont(18)
+  Script.setWidget(w)
+  Script.complete()
+} else {
+  // make table
+  let table = new UITable()
+  
+  // add header
+  let row = new UITableRow()
+  row.isHeader = true
+  row.addText(`Coronavirus Stats in ${country}`)
+  table.addRow(row)
+  
+  // fill data
+  table.addRow(createRow("Cases", res.cases))
+  table.addRow(createRow("Today", res.todayCases))
+  table.addRow(createRow("Deaths", res.deaths))
+  table.addRow(createRow("Recovered", res.recovered))
+  table.addRow(createRow("Critical", res.critical))
+  
+  if (config.runsWithSiri)
+    Speech.speak(`There are ${res.cases} cases in ${country}, and ${res.todayCases} cases today.`)
+  
+  // present table
+  table.present()
+}
+
+function createRow(title, number) {
+  let row = new UITableRow()
+  row.addText(title)
+  row.addText(number.toString()).rightAligned()
+  return row
+}
---
+++ MOMA.js
@@ -0,0 +1,52 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: light-gray; icon-glyph: magic;
+//Fetches Random Images from the metmuseum-api ; Stores the ids in the keychain in order to prevent multiple calls
+//Department ID = 11 is paintings ; use other if you wish
+var keychainkey = "paintingIDs"
+var res
+if(!Keychain.contains(keychainkey)) {
+  console.log("Keychainentry does not exist... create it");
+  const url = 'https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&medium=Paintings&departmentId=11&q=Painting'
+  const req = new Request(url)
+  res = await req.loadJSON()
+  let stringified = JSON.stringify(res)
+  Keychain.set(keychainkey, stringified)
+}
+else {
+  console.log("Using keychainentry now")
+  var idsFromKeychain = Keychain.get(keychainkey)
+//   console.log(idsFromKeychain)
+  res = JSON.parse(idsFromKeychain)
+//   console.log(res)
+}
+const max = res.total-1
+console.log("Max: "+max);
+const min = 0
+const random = Math.floor(Math.random() * (max - min + 1)) + min
+console.log("Random: "+random);
+const picked = res.objectIDs[random]
+console.log("Picked: "+picked);
+const req2 = new Request('https://collectionapi.metmuseum.org/public/collection/v1/objects/'+picked)
+// const req2 = new Request("https://collectionapi.metmuseum.org/public/collection/v1/objects/436978")
+const res2 = await req2.loadJSON()
+const i = new Request(res2.primaryImageSmall);
+const img = await i.loadImage();
+
+let widget = createWidget(img, res2.objectURL)
+if (config.runsInWidget) {
+  // create and show widget
+  Script.setWidget(widget)
+  Script.complete()
+}
+else {
+  widget.presentLarge()
+}
+
+
+function createWidget(img, widgeturl) {
+  let w = new ListWidget()
+  w.backgroundImage = img
+  w.url = widgeturl
+  return w
+}
